c4c354c8c5ae4004fd9bb1afa44722f8efb773a4
Atsushi Watanabe <atsushi.w@ieee.org>
2016-08-06 19:56:42 +0900

    Added UDP_CSR check with timeout

--- at91lib/usb/device/core/USBD_UDP.c
+++ at91lib/usb/device/core/USBD_UDP.c
@@ -116,26 +116,38 @@ void USBD_InterruptHandler(void) RAMFUNC;
 /// \param endpoint The endpoint number of the CSR to process.
 /// \param flags The bitmap to set to 1.
 #define SET_CSR(endpoint, flags) \
-    { \
-        volatile unsigned int reg; \
-        reg = AT91C_BASE_UDP->UDP_CSR[endpoint] ; \
-        reg |= REG_NO_EFFECT_1_ALL; \
-        reg |= (flags); \
-        AT91C_BASE_UDP->UDP_CSR[endpoint] = reg; \
-    }
+	do { \
+		volatile unsigned int reg; \
+		reg = AT91C_BASE_UDP->UDP_CSR[endpoint] ; \
+		reg |= REG_NO_EFFECT_1_ALL; \
+		reg |= (flags); \
+		AT91C_BASE_UDP->UDP_CSR[endpoint] = reg; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) == (flags) ) break; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) == (flags) ) break; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) == (flags) ) break; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) == (flags) ) break; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) == (flags) ) break; \
+		/*TRACE_ERROR("SET_CSR Timedout\n\r");*/ \
+	} while (0);
         //while ( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) != (flags)); 
 
 /// Sets the specified bit(s) in the UDP_CSR register.
 /// \param endpoint The endpoint number of the CSR to process.
 /// \param flags The bitmap to clear to 0.
 #define CLEAR_CSR(endpoint, flags) \
-    { \
-        volatile unsigned int reg; \
-        reg = AT91C_BASE_UDP->UDP_CSR[endpoint]; \
-        reg |= REG_NO_EFFECT_1_ALL; \
-        reg &= ~(flags); \
-        AT91C_BASE_UDP->UDP_CSR[endpoint] = reg; \
-    }
+	do { \
+		volatile unsigned int reg; \
+		reg = AT91C_BASE_UDP->UDP_CSR[endpoint]; \
+		reg |= REG_NO_EFFECT_1_ALL; \
+		reg &= ~(flags); \
+		AT91C_BASE_UDP->UDP_CSR[endpoint] = reg; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) != (flags) ) break; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) != (flags) ) break; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) != (flags) ) break; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) != (flags) ) break; \
+		if( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) != (flags) ) break; \
+		/*TRACE_ERROR("CLEAR_CSR Timedout\n\r");*/ \
+	} while (0);
         //while ( (AT91C_BASE_UDP->UDP_CSR[endpoint] & (flags)) == (flags)); 
 //------------------------------------------------------------------------------
 
