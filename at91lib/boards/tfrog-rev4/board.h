ddc49fee98b40780f49fb8f5ac8b9edd6d02465f
Atsushi Watanabe <atsushi.w@ieee.org>
2023-06-29 13:30:49 +0900

    Apply to some dir under at91lib

--- at91lib/boards/tfrog-rev4/board.h
+++ at91lib/boards/tfrog-rev4/board.h
@@ -30,10 +30,10 @@
 //------------------------------------------------------------------------------
 /// ¥dir
 /// !Purpose
-/// 
+///
 /// Definition and functions for using AT91SAM7SE-related features, such
 /// has PIO pins, memories, etc.
-/// 
+///
 /// !Usage
 /// -# The code for booting the board is provided by board_cstartup.S and
 ///    board_lowlevel.c.
@@ -41,14 +41,14 @@
 ///    components, see board.h.
 /// -# For manipulating memories (remapping, SDRAM, etc.), see board_memories.h.
 //------------------------------------------------------------------------------
- 
+
 //------------------------------------------------------------------------------
 /// ¥unit
 /// !Purpose
-/// 
+///
 /// Definition of AT91SAM7SE-EK characteristics, AT91SAM7SE-dependant PIOs and
 /// external components interfacing.
-/// 
+///
 /// !Usage
 /// -# For operating frequency information, see "SAM7SE-EK - Operating frequencies".
 /// -# For using portable PIO definitions, see "SAM7SE-EK - PIO definitions".
@@ -57,17 +57,17 @@
 /// -# For memory-related definitions, see "SAM7SE-EK - Memories".
 //------------------------------------------------------------------------------
 
-#ifndef BOARD_H 
-#define BOARD_H 
+#ifndef BOARD_H
+#define BOARD_H
 
 //------------------------------------------------------------------------------
 //         Headers
 //------------------------------------------------------------------------------
 
 #if defined(at91sam7se512)
-    #include "at91sam7se512/AT91SAM7SE512.h"
+#include "at91sam7se512/AT91SAM7SE512.h"
 #else
-    #error Board does not support the specified chip.
+#error Board does not support the specified chip.
 #endif
 
 //------------------------------------------------------------------------------
@@ -82,44 +82,42 @@
 /// - BOARD_NAME
 
 /// Name of the board.
-#define BOARD_NAME "T-frog Motor Driver rev.4" 
+#define BOARD_NAME "T-frog Motor Driver rev.4"
 /// Board definition.
-#define theva_rev1 
-#define tfrog_rev1 
-#define tfrog_rev4 
+#define theva_rev1
+#define tfrog_rev1
+#define tfrog_rev4
 /// Family definition.
-#define at91sam7se 
+#define at91sam7se
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 /// ¥page "SAM7SE-EK - Operating frequencies"
 /// This page lists several definition related to the board operating frequency
 /// (when using the initialization done by board_lowlevel.c).
-/// 
+///
 /// !Definitions
 /// - BOARD_MAINOSC
 /// - BOARD_MCK
 
 /// Frequency of the board main oscillator.
-#define BOARD_MAINOSC           18432000
+#define BOARD_MAINOSC 18432000
 
 /// Master clock frequency (when using board_lowlevel.c).
-#define BOARD_MCK               48000000
+#define BOARD_MCK 48000000
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 // ADC
 //------------------------------------------------------------------------------
 /// ADC clock frequency, at 10-bit resolution (in Hz)
-#define ADC_MAX_CK_10BIT         5000000
+#define ADC_MAX_CK_10BIT 5000000
 /// ADC clock frequency, at 8-bit resolution (in Hz)
-#define ADC_MAX_CK_8BIT          8000000
+#define ADC_MAX_CK_8BIT 8000000
 /// Startup time max, return from Idle mode (in ｵs)
-#define ADC_STARTUP_TIME_MAX       20
+#define ADC_STARTUP_TIME_MAX 20
 /// Track and hold Acquisition Time min (in ns)
-#define ADC_TRACK_HOLD_TIME_MIN   600
-
-
+#define ADC_TRACK_HOLD_TIME_MIN 600
 
 #define BOARD_FLASH_EFC
 
@@ -127,7 +125,7 @@
 /// ¥page "SAM7SE-EK - USB device"
 /// This page lists constants describing several characteristics (controller
 /// type, D+ pull-up type, etc.) of the USB device controller of the chip/board.
-/// 
+///
 
 /// Chip has a UDP controller.
 #define BOARD_USB_UDP
@@ -136,16 +134,16 @@
 #define BOARD_USB_PULLUP_INTERNAL
 
 /// Number of endpoints in the USB controller.
-#define BOARD_USB_NUMENDPOINTS                  8
+#define BOARD_USB_NUMENDPOINTS 8
 
 /// Returns the maximum packet size of the given endpoint.
-#define BOARD_USB_ENDPOINTS_MAXPACKETSIZE(i)    (((i == 4) || (i == 5)) ? 512 : 64)
+#define BOARD_USB_ENDPOINTS_MAXPACKETSIZE(i) (((i == 4) || (i == 5)) ? 512 : 64)
 
 /// Returns the number of FIFO banks for the given endpoint.
-#define BOARD_USB_ENDPOINTS_BANKS(i)            (((i == 0) || (i == 3)) ? 1 : 2)
+#define BOARD_USB_ENDPOINTS_BANKS(i) (((i == 0) || (i == 3)) ? 1 : 2)
 
 /// USB attributes configuration descriptor (bus or self powered, remote wakeup)
-#define BOARD_USB_BMATTRIBUTES                  USBConfigurationDescriptor_SELFPOWERED_NORWAKEUP
+#define BOARD_USB_BMATTRIBUTES USBConfigurationDescriptor_SELFPOWERED_NORWAKEUP
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
@@ -157,56 +155,99 @@
 ///
 
 /// DBGU pins (DTXD and DRXD) definitions.
-#define PINS_DBGU  {0x00000600, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
+#define PINS_DBGU                                                         \
+  {                                                                       \
+    0x00000600, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT \
+  }
 
 /// LED #0 pin definition.
-#define PIN_LED_0  {1 << 8, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT}
+#define PIN_LED_0                                                     \
+  {                                                                   \
+    1 << 8, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT \
+  }
 /// LED #1 pin definition.
-#define PIN_LED_1  {1 << 31, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_OUTPUT_1, PIO_DEFAULT}
+#define PIN_LED_1                                                      \
+  {                                                                    \
+    1 << 31, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_OUTPUT_1, PIO_DEFAULT \
+  }
 /// LED #2 pin definition.
-#define PIN_LED_2  {1 << 30, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_OUTPUT_1, PIO_DEFAULT}
+#define PIN_LED_2                                                      \
+  {                                                                    \
+    1 << 30, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_OUTPUT_1, PIO_DEFAULT \
+  }
 /// List of all LEDs pin definitions.
-#define PINS_LEDS  PIN_LED_0, PIN_LED_1, PIN_LED_2
+#define PINS_LEDS PIN_LED_0, PIN_LED_1, PIN_LED_2
 
 /// ADC_AD0 pin definition.
-#define PIN_ADC_ADC0 {1 << 17, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_PULLUP}
+#define PIN_ADC_ADC0                                               \
+  {                                                                \
+    1 << 17, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_PULLUP \
+  }
 /// ADC_AD1 pin definition.
-#define PIN_ADC_ADC1 {1 << 18, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_PULLUP}
+#define PIN_ADC_ADC1                                               \
+  {                                                                \
+    1 << 18, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_PULLUP \
+  }
 /// ADC_AD2 pin definition.
-#define PIN_ADC_ADC2 {1 << 19, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_PULLUP}
+#define PIN_ADC_ADC2                                               \
+  {                                                                \
+    1 << 19, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_PULLUP \
+  }
 /// ADC_AD3 pin definition.
-#define PIN_ADC_ADC3 {1 << 20, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_PULLUP}
+#define PIN_ADC_ADC3                                               \
+  {                                                                \
+    1 << 20, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_PULLUP \
+  }
 /// Pins ADC
 #define PINS_ADC PIN_ADC_ADC0, PIN_ADC_ADC1, PIN_ADC_ADC2, PIN_ADC_ADC3
 
 /// USB VBus monitoring pin definition.
-#define PIN_USB_VBUS    {1 << 19, AT91C_BASE_PIOC, AT91C_ID_PIOC, PIO_INPUT, PIO_PULLUP}
+#define PIN_USB_VBUS                                               \
+  {                                                                \
+    1 << 19, AT91C_BASE_PIOC, AT91C_ID_PIOC, PIO_INPUT, PIO_PULLUP \
+  }
 
-#define PINS_SRAM  \
-    {0x000000FE, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_PERIPH_B, PIO_DEFAULT}, \
-    {0x0000FFFF, AT91C_BASE_PIOC, AT91C_ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, \
-    {0x00E00000, AT91C_BASE_PIOC, AT91C_ID_PIOC, PIO_PERIPH_B, PIO_DEFAULT}
+#define PINS_SRAM                                                              \
+  {0x000000FE, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_PERIPH_B, PIO_DEFAULT},     \
+      {0x0000FFFF, AT91C_BASE_PIOC, AT91C_ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, \
+  {                                                                            \
+    0x00E00000, AT91C_BASE_PIOC, AT91C_ID_PIOC, PIO_PERIPH_B, PIO_DEFAULT      \
+  }
 
-#define PINS_USERIO  \
-    {0x0021C701, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_INPUT, PIO_PULLUP}
+#define PINS_USERIO                                                   \
+  {                                                                   \
+    0x0021C701, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_INPUT, PIO_PULLUP \
+  }
 
-#define PINS_RS485  {0x000000E0, AT91C_BASE_PIOA, AT91C_ID_US0, PIO_PERIPH_A, PIO_PULLUP}
+#define PINS_RS485                                                      \
+  {                                                                     \
+    0x000000E0, AT91C_BASE_PIOA, AT91C_ID_US0, PIO_PERIPH_A, PIO_PULLUP \
+  }
 //------------------------------------------------------------------------------
 
-#define PIN_PWM_ENABLE    {1 << 16, AT91C_BASE_PIOC, AT91C_ID_PIOC, PIO_OUTPUT_1, PIO_DEFAULT}
+#define PIN_PWM_ENABLE                                                 \
+  {                                                                    \
+    1 << 16, AT91C_BASE_PIOC, AT91C_ID_PIOC, PIO_OUTPUT_1, PIO_DEFAULT \
+  }
 
-#define PIN_PWM_CYCLE     {1 << 19, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_INPUT, PIO_PULLUP}
-#define PIN_PWM_CYCLE2    {1 << 20, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_PERIPH_A, PIO_PULLUP}
-#define PIN_PCK_PCK1      {1 << 17, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_PERIPH_A, PIO_DEFAULT}
+#define PIN_PWM_CYCLE                                              \
+  {                                                                \
+    1 << 19, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_INPUT, PIO_PULLUP \
+  }
+#define PIN_PWM_CYCLE2                                                \
+  {                                                                   \
+    1 << 20, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_PERIPH_A, PIO_PULLUP \
+  }
+#define PIN_PCK_PCK1                                                   \
+  {                                                                    \
+    1 << 17, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_PERIPH_A, PIO_DEFAULT \
+  }
 
+#define RAMFUNC __attribute__((long_call, section(".ramfunc")))
 
-#define RAMFUNC __attribute__ ((long_call, section (".ramfunc")))
+#define FLASH_USERDATA_START ((char*)0x0017F800)
 
-#define FLASH_USERDATA_START   ((char*)0x0017F800)
+#define VSRC_DIV 10 / 84
 
-
-#define VSRC_DIV         10/84
-
-
-#endif //#ifndef BOARD_H
+#endif  //#ifndef BOARD_H
 
